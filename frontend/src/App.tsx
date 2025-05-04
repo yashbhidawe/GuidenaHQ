0117b4144e6a81fd63e4f0234b1031677e7bb27b
yash bhidawe <ybh5179@gmail.com>
2025-05-04 21:42:30 +0530

    fix: vercel issue

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,16 @@ const App = () => {
     <div className="text-4xl flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-deep-teal">GuidenaHQ is coming soon!</h1>
       <h2 className="text-2xl text-gray-700">
-        Excitement is: Excientment * {count}
+        Excitement is: Excientment X {count}
       </h2>
-      <Button variant="default"> Excited? </Button>
+      <Button
+        variant="default"
+        onClick={() => setCount(count + 1)}
+        className="mt-2 cursor-pointer"
+      >
+        {" "}
+        Excited?{" "}
+      </Button>
     </div>
   );
 };
