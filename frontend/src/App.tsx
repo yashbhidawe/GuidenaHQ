7934f9b9253c46a04de1434674763ebb4eb3b437
yash bhidawe <ybh5179@gmail.com>
2025-05-04 21:31:15 +0530

    Add: shadCN, react-router-dom

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div className="text-4xl flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-deep-teal">GuidenaHQ is coming soon!</h1>
+      <h2 className="text-2xl text-gray-700">
+        Excitement is: Excientment * {count}
+      </h2>
+      <Button variant="default"> Excited? </Button>
+    </div>
+  );
+};
+
+export default App;
