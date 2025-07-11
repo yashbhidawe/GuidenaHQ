b1f59f8ccd998e783b7a3ba146135f3c10aacd17
Yash Bhidawe <ybh5179@gmail.com>
2025-07-11 23:57:15 +0530

    Test: Pre-production 1

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:5173"; //for local
-// export const BASE_URL = "https://guidena-hq.vercel.app"; //for dev
+// export const BASE_URL = "http://localhost:5173";
+//for local
+export const BASE_URL = "https://guidena-hq.vercel.app";
+// //for dev
