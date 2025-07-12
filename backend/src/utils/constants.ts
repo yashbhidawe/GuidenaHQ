195424fcc67082971230e14f56ba2c623be2dd95
Yash Bhidawe <ybh5179@gmail.com>
2025-07-12 08:02:09 +0530

    fix: base url

--- backend/src/utils/constants.ts
+++ backend/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:5173";
 //for local
-export const BASE_URL = "https://guidena-hq.vercel.app";
+export const BASE_URL = "https://guidenahq.vercel.app";
 // //for dev
